414426dc4cf4ff4b6a8113f8f292ab855049aa3e
Enzo Khamprasong <22503774@me.uvsq.fr>
2026-03-26 11:47:02 +0100

    Création et ajout de tous les tests de la soustraction

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -14,6 +14,18 @@ class PyCalcUnitTests(unittest.TestCase):
         got = calculator.add(l, r)
         self.assertEqual(got, e)
 
+  def test_subtract(self):
+    left = [50,50,50,50,50,-50,-50,-50,-50,-50,0,0,0]
+    right = [20,60,50,-50,0,60,40,-60,-50,0,50,-50,0]
+    expected = [30,-10,0,100,50,-110,-10,10,0,-50,-50,50,0]
+    for i in range(len(left)):
+      l = left[i]
+      r = right[i]
+      e = expected[i]
+      with self.subTest(l=l, r=r, e=e):
+        got = calculator.subtract(l, r)
+        self.assertEqual(got, e)
+  
 
 
 if __name__ == '__main__':
