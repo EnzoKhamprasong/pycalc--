e8f8064f672bd83d40b1d2b03fbf68c6649202d6
Enzo Khamprasong <22503774@me.uvsq.fr>
2026-03-26 11:43:30 +0100

    Ajout de tous les tests de l'addition

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -3,9 +3,9 @@ import pycalc.pycalc as calculator
 
 class PyCalcUnitTests(unittest.TestCase):
   def test_add(self):
-    left = [5]
-    right = [9]
-    expected = [14]
+    left = [50,50,50,50,50,-50,-50,-50,-50,-50,0,0,0]
+    right = [50,-10,-60,-50,0,60,40,50,-50,0,50,-50,0]
+    expected = [100,40,-10,0,50,10,-10,0,-100,-50,50,-50,0]
     for i in range(len(left)):
       l = left[i]
       r = right[i]
@@ -13,6 +13,8 @@ class PyCalcUnitTests(unittest.TestCase):
       with self.subTest(l=l, r=r, e=e):
         got = calculator.add(l, r)
         self.assertEqual(got, e)
+
+
 
 if __name__ == '__main__':
   unittest.main()
